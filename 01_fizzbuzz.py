fe17183680792be1ffb223532c5e674e2622ef93
bitterButler <b.hudson.mark@gmail.com>
2023-08-29 19:29:30 +0200

    fifth commit: if-elif-else order change

--- 01_fizzbuzz.py
+++ 01_fizzbuzz.py
@@ -6,11 +6,11 @@
 
 num = 0
 for num in range(1, 101):
-    if num % 3 == 0:
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
         print("Fizz")
     elif num % 5 == 0:
         print("Bizz")
-    elif num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
     else:
         print(num)
